59122ec3e5816f0308dbbb02917af11cab75c07b
Alex De Meo <alexander.demeo@ocsbstudent.ca>
2019-10-25 13:58:42 -0400

    finished what i needed to do

--- SampleVideoGameAlex/main_menu.lua
+++ SampleVideoGameAlex/main_menu.lua
@@ -43,19 +43,19 @@ local instructionsButton
 
 -- Creating Transition Function to Credits Page
 local function CreditsTransition( )       
-    composer.gotoScene( "credits_screen", {effect = "flipFadeOutIn", time = 500})
+    composer.gotoScene( "credits_screen", {effect = "crossFade", time = 500})
 end 
 
 -----------------------------------------------------------------------------------------
 
 -- Creating Transition to Level1 Screen
 local function Level1ScreenTransition( )
-    composer.gotoScene( "level1_screen", {effect = "zoomInOutFade", time = 1000})
+    composer.gotoScene( "level1_screen", {effect = "fade", time = 1000})
 end    
 
 -- INSERT LOCAL FUNCTION DEFINITION THAT GOES TO INSTRUCTIONS SCREEN 
 local function InstructionsTransition( )
-    composer.gotoScene("instructions", {effect = "zoomInOutFade", time = 1000})
+    composer.gotoScene("instructions", {effect = "flip", time = 1000})
 end
 -----------------------------------------------------------------------------------------
 -- GLOBAL SCENE FUNCTIONS
